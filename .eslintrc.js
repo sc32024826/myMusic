b71a20df1c2d14d3408bb341d059e1d0ed1cdb24
Shencheng <shencheng_1014@aliyun.com>
2020-08-20 16:58:57 +0800

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,26 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    root: true,
+    env: {
+        node: true
+    },
+    extends: [
+        'plugin:vue/essential',
+        '@vue/standard'
+    ],
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'vue/no-parsing-error': [2, {
+            'x-invalid-end-tag': false
+        }],
+        'no-undef': 'off',
+        'camelcase': 'off',
+        'indent': ['off', 4],
+        'no-tabs': 'off',
+        "space-before-function-paren": 0,
+        'no-unused-expressions': 0
+    }
 }
